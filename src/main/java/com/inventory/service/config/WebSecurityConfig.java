6e7007057c8ff9d3be083caa128952ed666a943c
samarth.shah <samarth.shah@einfochips.com>
2023-01-05 00:25:50 +0530

    fix

--- src/main/java/com/inventory/service/config/WebSecurityConfig.java
+++ src/main/java/com/inventory/service/config/WebSecurityConfig.java
@@ -4,9 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-/**
- * Created by jt on 5/16/20.
- */
+
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
